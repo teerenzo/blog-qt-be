14dd6ac1e8b0ed9f07adf98ed10769a8c5fc56bb
teerenzo <renzahoemmanuel8@gmail.com>
2024-08-02 12:41:20 +0200

    fixing env

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,18 @@
 const path = require("path");
-const Env = require("node-env-file");
+const dotenv = require("dotenv");
+const fs = require("fs");
 
-Env(path.join("./.env"));
+const envPath = path.resolve("./.env");
+console.log("Loading environment variables from:", envPath);
 
+if (fs.existsSync(envPath)) {
+  console.log(".env file exists at", envPath);
+  dotenv.config({ path: envPath });
+} else {
+  console.error(".env file does not exist at", envPath);
+}
+
+// Output an environment variable to confirm loading
 console.log("DB_HOST:", process.env.DB_HOST);
 
 module.exports = {
@@ -23,5 +33,17 @@ module.exports = {
           },
   },
   test: {},
-  production: {},
+  production: {
+    username: `${process.env.DB_USERNAME}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_DATABASE}`,
+    host: `${process.env.DB_HOST}`,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
