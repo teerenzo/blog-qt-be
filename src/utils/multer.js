9b751cd0ab56a56abe3aa5dfa798765960d53774
teerenzo <renzahoemmanuel8@gmail.com>
2024-08-03 09:30:20 +0200

    fixing upload tmp folder

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -1,8 +1,16 @@
 import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const tmpDir = path.resolve("tmp");
+
+if (!fs.existsSync(tmpDir)) {
+  fs.mkdirSync(tmpDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./tmp/");
+    cb(null, tmpDir);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
